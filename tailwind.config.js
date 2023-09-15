24712b28dfe4707b3b92226a5d237302f1c8923b
Engr. R <r-softengr@gmail.com>
2023-09-16 04:08:32 +0800

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  content: ["./components/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {
       container: {
